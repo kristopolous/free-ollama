d69c1b08458fb540cd346c02c171374412c6f7f3
Chris McKenzie <kristopolous@yahoo.com>
2026-07-06 15:15:30 -0700

    adding fid

--- graflex/graflex.sh
+++ graflex/graflex.sh
@@ -9,18 +9,24 @@ case "$SERVICE" in
     PORTS="11434,10443,8085,10001,8080,80,443,1194,110,8983,28017,21,5060,5601"
     SERVERS="nginx,cloudflare,Apache"
     COUNTRIES="US,AU,IN,JP,DE,CA,RU,BR,CN,ID,IE,FR,ES,ID,IT,SE,PL"
+    FID="UsjT+U+5J1db7DhwVOaRww==,gcg43SR+B8fEyFZyAZswOw==,WG5T78I2x/yKvDq/9kLayA==,vWJAw3jTn47wQTBzzr6Y5A==,IddNyyDw+Bero+vJOQnxFQ=="
     ;;
+    
   comfyui)
     QUERY='title="ComfyUI"'
     PORTS="8188,8080,80,443"
     SERVERS="nginx,cloudflare"
     COUNTRIES="US,AU,IN,JP,DE,CA,BR,CN"
+    FID="2zn7oqmRiwaUu3+PzyTjvw==,7aBY0X9WxdeghtrJGx1MEQ==,MJ7K0wma6lKOVne5ksgrSw==,yhjkkd4AnCsogP9Ms1QgVA==
+"
     ;;
+
   a1111)
     QUERY='icon_hash="2075038152" && body="Stable Diffusion"'
     PORTS="7860,7861,8080,80,443,10000"
     SERVERS="nginx,cloudflare,uvicorn"
     COUNTRIES="US,AU,IN,JP,DE,ES,RU,CA,BR,CN,KR"
+    FID="4OeA79EXS7Z+DdzkAvrBag==,WPcuJSTXuzZQIeov/h9jgA==,NCPjfTODiuNabsua2LTY7Q==,SWCeWGsQp4gTPi4YvgrIdQ=="
     ;;
   *)
     echo "Usage: $0 [ollama|comfyui|a1111]" >&2
@@ -28,4 +34,4 @@ case "$SERVICE" in
     ;;
 esac
 
-exec ./graflex.py -q "$QUERY" -a fetch -n "$SERVICE" -p "$PORTS" --servers "$SERVERS" -c "$COUNTRIES"
+exec ./graflex.py -q "$QUERY" -f "$FID" -a fetch -n "$SERVICE" -p "$PORTS" --servers "$SERVERS" -c "$COUNTRIES"
