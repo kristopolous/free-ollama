375e682ad7d0e96a511e661ae29d57a82caf2290
Chris McKenzie <kristopolous@yahoo.com>
2026-07-18 01:03:20 -0700

    country list update

--- graflex/graflex.sh
+++ graflex/graflex.sh
@@ -10,7 +10,7 @@ case "$SERVICE" in
     QUERY="body='ollama is running'"
     PORTS="11434,10443,8085,10001,8080,80,443,1194,110,8983,28017,21,5060,5601"
     SERVERS="nginx,cloudflare,Apache"
-    COUNTRIES="TH,MX,MY,BH,SG,KR,GB,AE,US,AU,IN,JP,DE,CA,RU,IL,BR,CN,IE,FR,ES,ID,IT,CH,ZA,HK,PL"
+    COUNTRIES="TH,MX,MY,NZ,BH,SG,KR,GB,AE,US,AU,IN,JP,DE,CA,RU,IL,BR,CN,IE,FR,ES,ID,IT,CH,ZA,HK,PL"
     FID="UsjT+U+5J1db7DhwVOaRww==,gcg43SR+B8fEyFZyAZswOw==,WG5T78I2x/yKvDq/9kLayA==,vWJAw3jTn47wQTBzzr6Y5A==,IddNyyDw+Bero+vJOQnxFQ=="
     ;;
     
@@ -27,7 +27,7 @@ case "$SERVICE" in
     QUERY='icon_hash="2075038152" && body="Stable Diffusion"'
     PORTS="7860,7861,8080,80,443,10000"
     SERVERS="nginx,cloudflare,uvicorn"
-    COUNTRIES="AE,US,AU,IN,JP,DE,ES,RU,CA,BR,CN,KR"
+    COUNTRIES="TH,MX,MY,NZ,BH,SG,KR,GB,AE,US,AU,IN,JP,DE,CA,RU,IL,BR,CN,IE,FR,ES,ID,IT,CH,ZA,HK,PL"
     FID="4OeA79EXS7Z+DdzkAvrBag==,WPcuJSTXuzZQIeov/h9jgA==,NCPjfTODiuNabsua2LTY7Q==,SWCeWGsQp4gTPi4YvgrIdQ=="
     ;;
   *)
