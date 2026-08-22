43d2d70c0136c33ceb13bbf9ed01d08ad017592f
Chris McKenzie <kristopolous@yahoo.com>
2026-08-22 15:00:14 -0700

    additional ports and fids

--- graflex/graflex.sh
+++ graflex/graflex.sh
@@ -20,7 +20,7 @@ case "$SERVICE" in
 
   ollama)
     QUERY="body='ollama is running'"
-    PORTS="11434,10443,1025,8085,1024,1025,10001,10000,8080,8083,3000,16000,80,443,1194,110,8983,28017,21,5060,5601"
+    PORTS="11434,10443,1025,9443,9200,8085,1024,1025,10001,10000,8888,9000,8080,8083,3000,16000,80,443,1194,110,8983,28017,21,5060,5601"
     SERVERS="nginx,cloudflare,Apache"
     COUNTRIES="TH,FI,MX,TR,IR,AR,TW,MA,MY,NZ,BH,SG,AT,KR,PE,GB,AE,US,NL,AU,IN,VN,JP,DE,CA,RU,IL,BR,BE,CN,IE,FR,UA,ES,ID,IT,CH,ZA,HK,PL"
     FID=$({ 
@@ -30,6 +30,7 @@ cat << ENDL
     3Q7By8tPfRuNHpFiq0KrqA==
     3mGLf50yvyPWCWfSET9hDw==
     3wJ8qv5ND0OwkVt/Kjq4iQ==
+    4vh6LwKdiSJrB3Lrrkt3LA==
     65wLyEcpOlDyDW4X4g0gZw==
     AIB814ld1H6MvBd+6OPcCA==
     AeWKZ15RmMWZotdLMDDv4w==
@@ -37,6 +38,7 @@ cat << ENDL
     IQHpmTiP8/HzRTww3q1YrQ==
     IddNyyDw+Bero+vJOQnxFQ==
     J9855+VtkRUZ4OjziYwE/g==
+    KLZTw2V6Q8F48jRQERILuA==
     QtId0cK1hK63W7qg3BQGxg==
     RB7MvyQI2lfLDtfXp/oHFA==
     TrVIddAZ1noJcqqjy3K0tQ==
@@ -49,6 +51,7 @@ cat << ENDL
     hct4Naj+LBOYmss/EnqFgg==
     hoN2c4Ox+oKTWIv5RyANJQ==
     jq1/FUEPBJ5UKh2LOcVWZQ==
+    It1vVVEF3pCaBknoGd6c2g==
     nqM+fMcnax/qO2rEglJ72w==
     peMs7fD92YTW7k1jkyd9QQ==
     rI+VQgK8I9EBO92ugxTRhA==
